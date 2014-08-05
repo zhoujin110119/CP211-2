7bdb8a3b35bd3e8fc3be7094c10b1237df0d4d7e
zhoujin110110 <zhoujinandrew@126.com>
2014-08-05 18:13:04 +0800

    1234

---
+++ Matchismo/Matchismo/Model/CardPlayLogic.m
@@ -0,0 +1,59 @@
+//
+//  CardPlayLogic.m
+//  Matchismo
+//
+//  Created by Neo on 8/5/14.
+//  Copyright (c) 2014 Paradigm X. All rights reserved.
+//
+
+#import "CardPlayLogic.h"
+#import "PlayingCardDeck.h"
+
+@interface CardPlayLogic()
+@property (nonatomic, readwrite) NSInteger score;
+@property (nonatomic, strong) NSMutableArray *cards;
+@property (nonatomic, strong) Deck *deck;
+@end
+
+@implementation CardPlayLogic
+
+- (PlayingCardDeck *)createDeck
+{
+    return [[PlayingCardDeck alloc] init];
+}
+
+- (Deck *)deck
+{
+    if (! _deck) _deck = [self createDeck];
+    return _deck;
+}
+
+-(NSMutableArray *) cards{
+	if(_cards) _cards = [[NSMutableArray alloc] init];
+	return _cards;
+}
+-(instancetype) init:(NSUInteger) count{
+	self = [super init];
+	
+	if(self){
+		for(int i = 0; i < count; i++){
+			Card *card = [self.deck drawRandomCard];
+			[self.cards addObject:card];
+		}
+	}
+	return self;
+}
+
+-(void)selectCard:(NSUInteger) index{
+	Card *card = [self cardAtIndex:index];
+	
+
+}
+
+
+
+
+
+
+
+@end
